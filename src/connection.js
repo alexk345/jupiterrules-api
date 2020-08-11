bb1d965c29cd338c365617800bfa8c4fcffa19c2
alexk345 <alexk345@Gmail.com>
2020-08-11 03:22:27 -0700

    222

--- src/connection.js
+++ src/connection.js
@@ -4,13 +4,13 @@ const User = require("./User.model");
 
 mongoose.Promise = global.Promise;
  
-const connection = "mongodb://srv-captain--mongo:27017/demotest&w=1";	
+const connection = "mongodb://srv-captain--mongo:27017/demotest";	
 
 
 
 
 const connectDb = () => {
-  return mongoose.connect(connection,{ useNewUrlParser: true,auth:{authdb:"admin"} })	  
+  return mongoose.connect(connection,{ useNewUrlParser: true})	  
   .then(function (db) { // <- db as first argument
     console.log(db)
     console.log("=========================")
