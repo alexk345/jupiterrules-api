7b11aa2771b3cfc2f5cbae10d4f1ba55a7f65f63
alexk345 <alexk345@Gmail.com>
2020-08-10 22:26:28 -0700

    22223

--- src/connection.js
+++ src/connection.js
@@ -4,8 +4,7 @@ const User = require("./User.model");
 
 mongoose.Promise = global.Promise;
  
- /*
-const connection = "mongodb://root:Test345@srv-captain--mongo:27017/mongo-test";	
+const connection = "mongodb://srv-captain--mongo:27017/mongo-test";	
 
 const connectDb = () => {	
   return mongoose.connect(connection,{ useNewUrlParser: true });	  
@@ -16,21 +15,7 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log('connected')
 });
-*/
 
-let mongoConnectionLocal = {
-  'url':`mongodb://srv-captain--mongo:27017/mongo-test";`
-  };
-  mongoose.connect(mongoConnectionLocal.url, {auth:{authdb:"admin"}}, err => { if(err) { console.log(err); }});
-  
-  var options = {
-  auth: {authdb: 'admin'},
-  user: process.env.MongoDBLocalUser,
-  pass: process.env.MongoDBLocalPassword,
-  }
- 
-  const connectDb = mongoose.connect(mongoConnectionOnline.url, options, err => { if(err) { console.log(err); }});
-  
 
 
 module.exports = connectDb;
