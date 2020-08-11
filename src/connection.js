8e72a009c0f66002bbed41b3065eaf816e7e5815
alexk345 <alexk345@Gmail.com>
2020-08-10 19:13:24 -0700

    dfdf

--- src/connection.js
+++ src/connection.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const User = require("./User.model");
 
+mongoose.Promise = global.Promise;
 
  
 const connection = "mongodb://root:#Test345@srv-captain--mongo:27017/mongo-test";
