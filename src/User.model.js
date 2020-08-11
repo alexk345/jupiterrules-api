640d37e0be7846ba1f4077c17e05e44c24c61f81
alexk345 <alexk345@Gmail.com>
2020-08-11 03:01:23 -0700

    qqq

--- src/User.model.js
+++ src/User.model.js
@@ -1,5 +1,5 @@
 //const mongoose = require("mongoose");
-const connection = require("connection.js");
+const connection = require("./connection.js");
 
 const userSchema = new mongoose.Schema({
   username: {
