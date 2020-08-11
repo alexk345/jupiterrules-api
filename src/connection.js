a5dbd34776412f51bd906418951701d3e52f750e
alexk345 <alexk345@Gmail.com>
2020-08-10 20:14:57 -0700

    qwqw

--- src/connection.js
+++ src/connection.js
@@ -4,39 +4,12 @@ const User = require("./User.model");
 
 mongoose.Promise = global.Promise;
 
- 
-const connection = "mongodb://srv-captain--mongo:27017/mongo-test";
 
+const connection = "mongodb://srv-captain--mongo:27017/mongo-test";	
 
-
-  
-
-// Connecting to the database
-const connect = async function () {
-  var options = {
-    //user: "root",
-    //pass: "#Test345",
-    useNewUrlParser: true,
-    keepAlive:true 
-  };
-
-  const uri = connection; // Will return DB URI 
-  console.log(`Connecting to DB - uri: ${uri}`);
-  return mongoose.connect(uri, options);
+const connectDb = () => {	
+  return mongoose.connect(connection,{ useNewUrlParser: true });	  
 };
 
 
-(async () => {
-  try {
-   const connected = await connect();
-   
-  module.exports = connected;
-
-  } catch(e) {
-   console.log('Error happend while connecting to the DB: ', e.message)
-  }
-})();
-
-
-
- 
+module.exports = connectDb;
